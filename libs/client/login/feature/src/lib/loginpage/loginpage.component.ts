afa96b1ddeb3b4d788567c7bf6acd612e76993fe
MatthewGotte <u20734621@tuks.co.za>
2022-05-05 22:39:14 +0200

    :fire: Adding libs, Adding Routing

---
+++ libs/client/login/feature/src/lib/loginpage/loginpage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'training-buddy-loginpage',
+  templateUrl: './loginpage.component.html',
+  styleUrls: ['./loginpage.component.scss']
+})
+export class LoginpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ libs/client/signup/feature/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/client-signup-feature.module';
