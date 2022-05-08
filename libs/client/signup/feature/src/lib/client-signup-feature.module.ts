7bfaad9ad4fe0adbbd55d85b192ed61c4c47b9a7
MatthewGotte <u20734621@tuks.co.za>
2022-05-08 05:19:25 +0200

    :lipstick: Rough final signup and login forms with google auto complete and geo-coding

--- libs/client/signup/feature/src/lib/client-signup-feature.module.ts
+++ libs/client/signup/feature/src/lib/client-signup-feature.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 import { SignupComponent } from './signup/signup.component';
 import { SignupRoutingModule } from './signup-routing.module';
 import { UiModule } from '@training-buddy/client/shared/components/navbar/ui';
+import { MatGoogleMapsAutocompleteModule } from '@angular-material-extensions/google-maps-autocomplete';
+import { DataAccessModule } from '@training-buddy/client/shared/interfaces/user/data-access';
 
 @NgModule({
   imports: [
     CommonModule,
     SignupRoutingModule,
-    UiModule    
+    UiModule,
+    MatGoogleMapsAutocompleteModule,
   ],
   declarations: [
     SignupComponent,
