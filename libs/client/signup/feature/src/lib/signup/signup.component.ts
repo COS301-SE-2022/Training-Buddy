df0d767b63b5eec1c35b56c3f21d2c6a0d4439da
MatthewGotte <u20734621@tuks.co.za>
2022-05-08 05:19:25 +0200

    :lipstick: Rough final signup and login forms with google auto complete and geo-coding

--- libs/client/signup/feature/src/lib/signup/signup.component.ts
+++ libs/client/signup/feature/src/lib/signup/signup.component.ts
@@ -7,15 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  hide = true;
+  hide : boolean;
+  img : string;
 
-  constructor() { }
+  constructor() {
+    this.hide = true;
+    this.img = 'https://images.unsplash.com/photo-1607962837359-5e7e89f86776?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80';
+  }
 
   ngOnInit(): void {
+    return;
   }
 
   signup() {
     alert('register');
   }
+
+  /////////////////////////////////////////////////////////////////
+  //Google geocoding functions
+
+  //this function will log a comprehensive result from the search.
+  onAutocompleteSelected(event : any) {
+    console.log('22');
+    console.log(event);
+  }
+
+  //this function will just recieve the long and lat of the search query result.
+  onLocationSelected(event: any) {
+    const latitude = event.latitude;
+    const longitude = event.longitude;
+    console.log(`latitude: ${latitude}, longitude: ${longitude}`);
+  }
+
+  /////////////////////////////////////////////////////////////////
 
 }
