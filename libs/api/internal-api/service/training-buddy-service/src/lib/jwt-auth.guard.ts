0b30ca54cd03d78f615e99f5a23daf509f139c08
Muziwandile <84350184+muzicoding@users.noreply.github.com>
2022-05-10 09:34:45 +0200

    🐛 (API) Fixing Bugs and Adding comments

--- libs/api/internal-api/service/training-buddy-service/src/lib/jwt-auth.guard.ts
+++ libs/api/internal-api/service/training-buddy-service/src/lib/jwt-auth.guard.ts
@@ -3,6 +3,11 @@ import {GqlExecutionContext} from '@nestjs/graphql'
 import {AuthGuard} from '@nestjs/passport'
 @Injectable()
 export class JwtAuthGuard extends AuthGuard ('jwt'){
+  /**
+   * 
+   * @param context 
+   * @returns request body
+   */
   getRequest(context: ExecutionContext){
     const ctx = GqlExecutionContext.create(context);
     return ctx.getContext().req;
--- libs/api/internal-api/service/training-buddy-service/src/lib/login.guard.ts
+++ libs/api/internal-api/service/training-buddy-service/src/lib/login.guard.ts
@@ -8,6 +8,11 @@ export class LoginGuard extends AuthGuard ('local'){
  constructor(){
    super();
  }
+ /**
+  * 
+  * @param context 
+  * @returns RequestBody
+  */
  getRequest(context: ExecutionContext){
    const ctx = GqlExecutionContext.create(context);
    const request = ctx.getContext();
