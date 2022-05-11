433328e0c8f5b6faed5a50fe2165b28a0c78a545
Muziwandile <84350184+muzicoding@users.noreply.github.com>
2022-05-12 01:11:13 +0200

    🐛 (API): Bug Fix

--- libs/api/internal-api/api/training-buddy-api/src/lib/api-internal-api-api-training-buddy-api.module.ts
+++ libs/api/internal-api/api/training-buddy-api/src/lib/api-internal-api-api-training-buddy-api.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TrainingBuddyApiResolver } from './training-buddy-api.resolver';
 import { JwtModule } from '@nestjs/jwt';
+import { ApiInternalApiRepositoryDataAccessService } from '@training-buddy/api/internal-api/repository/data-access';
+import { PrismaService } from '@training-buddy/api/shared/services/prisma//data-access' ;
 import { TrainingBuddyServiceService, LoginGuard,ApiInternalApiServiceTrainingBuddyServiceModule  } from '@training-buddy/api/internal-api/service/training-buddy-service'
 @Module({
   controllers: [],
   imports: [ApiInternalApiServiceTrainingBuddyServiceModule],
-  providers: [TrainingBuddyApiResolver,TrainingBuddyServiceService, LoginGuard  ],
+  providers: [TrainingBuddyApiResolver,TrainingBuddyServiceService, LoginGuard, ApiInternalApiRepositoryDataAccessService ,PrismaService ],
   exports: [],
 })
 export class ApiInternalApiApiTrainingBuddyApiModule {}
