65ccee1b14dc5d718fe18a41b3ecd051baca0aab
MatthewGotte <u20734621@tuks.co.za>
2022-05-11 21:19:45 +0200

    :adhesive_bandage: Fixing link of navbar components

--- libs/client/shared/components/navbar/ui/src/lib/unsignednavbar/unsignednavbar.component.ts
+++ libs/client/shared/components/navbar/ui/src/lib/unsignednavbar/unsignednavbar.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'unsigned-navbar',
+  selector: 'training-buddy-unsigned-navbar',
   templateUrl: './unsignednavbar.component.html',
   styleUrls: ['./unsignednavbar.component.scss']
 })
 export class UnsignednavbarComponent implements OnInit {
 
-  constructor() { }
+  // constructor() { }
 
   ngOnInit(): void {
+    return;
   }
 
 }
