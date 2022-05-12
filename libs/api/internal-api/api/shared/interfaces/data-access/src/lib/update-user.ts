f05df0922d49795a5b7c317cdd1cf53c7382c1d7
Muziwandile <84350184+muzicoding@users.noreply.github.com>
2022-05-12 23:34:41 +0200

    ✨ (API): Update User Entity

---
+++ libs/api/internal-api/api/shared/interfaces/data-access/src/lib/update-user.ts
@@ -0,0 +1,20 @@
+
+import { Field , InputType} from '@nestjs/graphql'
+
+@InputType()
+export class UpdateUser {
+    @Field({nullable:true})
+    userName: string
+    @Field({nullable:true})
+    userSurname: string
+    @Field({nullable:true})
+    location:string
+    @Field({nullable:true})
+    email: string
+    @Field()
+    oldemail: string
+    @Field({nullable:true})
+    cellNumber: string
+    @Field({nullable:true})
+    password: string
+}
