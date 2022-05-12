9157fda1320647f7cb74d4dae5f2c2b6068367ef
MatthewGotte <u20734621@tuks.co.za>
2022-05-12 19:11:47 +0200

    :art: Completing the Athlete Statistics page

--- libs/client/athlete-profile/feature/src/lib/athleteprofile/athleteprofile.component.spec.ts
+++ libs/client/athlete-profile/feature/src/lib/athleteprofile/athleteprofile.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { UiModule } from '@training-buddy/client/shared/components/navbar/ui';
 import { Apollo } from 'apollo-angular';
-import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AthleteprofileComponent } from './athleteprofile.component';
 
