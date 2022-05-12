8780cccf7a65d176927ddbebdf0243c89afc8b69
sarah-a-king <94594759+sarah-a-king@users.noreply.github.com>
2022-05-12 19:10:12 +0200

    lmk

--- libs/api/internal-api/repository/data-access/src/lib/api-internal-api-repository-data-access.service.ts
+++ libs/api/internal-api/repository/data-access/src/lib/api-internal-api-repository-data-access.service.ts
@@ -1,11 +1,14 @@
 import { Injectable, Param } from '@nestjs/common';
+import { activityStatistic } from '@prisma/client';
 import { UserDto } from '@training-buddy/api/internal-api/api/shared/interfaces/data-access';
 import { PrismaService } from '@training-buddy/api/shared/services/prisma//data-access' ;
+import { emit } from 'process';
 
 @Injectable()
 export class ApiInternalApiRepositoryDataAccessService {
     constructor(private prisma: PrismaService) {}
 
+    //used for signing up
     async createUser(@Param() user:UserDto){
        return  await this.prisma.user.create({
             data: {
@@ -21,14 +24,48 @@ export class ApiInternalApiRepositoryDataAccessService {
         })
     }
 
-    async login(@Param() email:string):Promise<any> {
-        return await this.prisma.user.findUnique({
+    //used for logging in
+    async login(@Param() email:string){
+        await this.prisma.user.findUnique({
             where: {email:email}
+        }).then(async(value)=>{
+            console.log(value) ;
+            return value;
         })
     }
-    async findAll(@Param()location: string){
-        return await this.prisma.user.findMany({
+
+    //used to filter users by location
+    async findAll(@Param() location: string){
+        await this.prisma.user.findMany({
             where:{location: location}
+        }).then(async(value)=>{
+            console.log(value);
+            return value ;
+        })
+    }
+
+    //used to add an activity statistic for a specific user
+    async createActivityStatistic(@Param() stat:ActivityStatistic){
+        await this.prisma.activityStatistic.create({
+            data: {
+                activity: stat.activity ,
+                user: stat.email ,
+                experienceLevel: stat.XP ,
+                insight: stat.insight
+            }
+        }).then(async (value) => {
+            console.log(value) ;
+            return value ;
+        })
+    }
+
+    //retrieve all activity statistics for a user
+    async getAllActivityStatistics(@Param() userEmail: string){
+        await this.prisma.activityStatistic.findMany({
+            where:{user: userEmail}
+        }).then(async (value) => {
+            console.log(value) ;
+            return value ;
         })
     }
 }
