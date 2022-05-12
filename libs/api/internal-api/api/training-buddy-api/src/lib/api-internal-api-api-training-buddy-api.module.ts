6ce3ceedc4eb6412ae12645bcd067d196fc09b3c
Muziwandile <84350184+muzicoding@users.noreply.github.com>
2022-05-12 12:26:20 +0200

    👽️ (API): Sign UP Intergration

--- libs/api/internal-api/api/training-buddy-api/src/lib/api-internal-api-api-training-buddy-api.module.ts
+++ libs/api/internal-api/api/training-buddy-api/src/lib/api-internal-api-api-training-buddy-api.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TrainingBuddyApiResolver } from './training-buddy-api.resolver';
-import { JwtModule } from '@nestjs/jwt';
 import { ApiInternalApiRepositoryDataAccessService } from '@training-buddy/api/internal-api/repository/data-access';
 import { PrismaService } from '@training-buddy/api/shared/services/prisma//data-access' ;
+import {UserEntity } from '@training-buddy/api/internal-api/api/shared/interfaces/data-access';
 import { TrainingBuddyServiceService, LoginGuard,ApiInternalApiServiceTrainingBuddyServiceModule  } from '@training-buddy/api/internal-api/service/training-buddy-service'
 @Module({
   controllers: [],
   imports: [ApiInternalApiServiceTrainingBuddyServiceModule],
-  providers: [TrainingBuddyApiResolver,TrainingBuddyServiceService, LoginGuard, ApiInternalApiRepositoryDataAccessService ,PrismaService ],
+  providers: [TrainingBuddyApiResolver,TrainingBuddyServiceService, LoginGuard, ApiInternalApiRepositoryDataAccessService ,PrismaService,  UserEntity ],
   exports: [],
 })
 export class ApiInternalApiApiTrainingBuddyApiModule {}
