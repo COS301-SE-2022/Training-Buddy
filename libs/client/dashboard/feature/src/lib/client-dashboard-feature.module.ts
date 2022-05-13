6b0aa8ff440601d020f8a6cdabf2d8244e0b1ffd
MatthewGotte <u20734621@tuks.co.za>
2022-05-13 03:39:15 +0200

    Design for accept invite card

--- libs/client/dashboard/feature/src/lib/client-dashboard-feature.module.ts
+++ libs/client/dashboard/feature/src/lib/client-dashboard-feature.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UiModule } from '@training-buddy/client/shared/components/navbar/ui';
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { HttpClientJsonpModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
     CommonModule,
     DashboardRoutingModule,
-    UiModule
+    UiModule,
+    HttpClientJsonpModule,
   ],
   declarations: [
     DashboardComponent
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    theme: {
+      extend: {
+        backgroundImage: {
+        }
+      }
+    }
+  }
