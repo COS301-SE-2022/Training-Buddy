fc958bf1c856fc96bb1fab5954e1470ebb379ac1
MatthewGotte <u20734621@tuks.co.za>
2022-05-13 04:35:42 +0200

    Fixing imports for routing spec

--- libs/client/shared/components/navbar/ui/src/lib/navbar/navbar.component.spec.ts
+++ libs/client/shared/components/navbar/ui/src/lib/navbar/navbar.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { NavbarComponent } from './navbar.component';
 
 describe('NavbarComponent', () => {
@@ -8,7 +8,10 @@ describe('NavbarComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NavbarComponent ]
+      declarations: [ NavbarComponent ],
+      imports:[
+        RouterTestingModule
+      ]
     })
     .compileComponents();
   });
