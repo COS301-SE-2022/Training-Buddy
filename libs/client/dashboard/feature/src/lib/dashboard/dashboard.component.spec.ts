69ca45b69c5eb420b9feadf54f52b9155d11d3d8
MatthewGotte <u20734621@tuks.co.za>
2022-05-13 04:15:29 +0200

    Fixing Routing

--- libs/client/dashboard/feature/src/lib/dashboard/dashboard.component.spec.ts
+++ libs/client/dashboard/feature/src/lib/dashboard/dashboard.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { DashboardComponent } from './dashboard.component';
 
 describe('DashboardComponent', () => {
@@ -8,7 +8,10 @@ describe('DashboardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [ DashboardComponent ],
+      imports: [
+        RouterTestingModule
+      ]
     })
     .compileComponents();
   });
