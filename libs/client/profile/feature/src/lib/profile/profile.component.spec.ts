2d0fc6d73f4b5fec742c719a1142b415417cb391
takumuguti <90533439+takumuguti@users.noreply.github.com>
2022-05-13 02:18:04 +0200

    ✅fix tests

--- libs/client/profile/feature/src/lib/profile/profile.component.spec.ts
+++ libs/client/profile/feature/src/lib/profile/profile.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { UiModule } from '@training-buddy/client/shared/components/navbar/ui';
 
 import { ProfileComponent } from './profile.component';
@@ -13,7 +14,8 @@ describe('ProfileComponent', () => {
       declarations: [ ProfileComponent ],
       imports: [
         ReactiveFormsModule,
-        UiModule
+        UiModule,
+        NoopAnimationsModule
       ]
     })
     .compileComponents();
