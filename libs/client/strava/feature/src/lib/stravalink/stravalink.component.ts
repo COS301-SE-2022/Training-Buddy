f86b31db99d2a567950c84a5b3322833c5b5777b
MatthewGotte <u20734621@tuks.co.za>
2022-05-25 01:06:24 +0200

    strava/link complete

--- libs/client/strava/feature/src/lib/stravalink/stravalink.component.ts
+++ libs/client/strava/feature/src/lib/stravalink/stravalink.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StravalinkComponent implements OnInit {
 
-  constructor() { }
+  img : string
+
+  constructor() {
+    this.img = 'https://images.unsplash.com/photo-1530143311094-34d807799e8f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2669&q=80';
+  }
 
   ngOnInit(): void {
   }
