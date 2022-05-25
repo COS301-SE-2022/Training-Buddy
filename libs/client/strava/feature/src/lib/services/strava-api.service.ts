961bf88a956af6d5e12066baf550202e266c626c
MatthewGotte <u20734621@tuks.co.za>
2022-05-25 02:25:36 +0200

    working strava OAuth
    Almost all code is complete for the strava linking, just some small error validation needed on the strava/link page during async function execution to inform the user what is going on.

--- libs/client/strava/feature/src/lib/services/strava-api.service.ts
+++ libs/client/strava/feature/src/lib/services/strava-api.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -7,7 +8,7 @@ export class StravaAPIService {
 
   client_id = '85093';
   client_secret = 'a104b3a3699b54450312f26698167e28b61d5624';
-  grant_type = 'authorization_token';
+  grant_type = 'authorization_code';
 
   OAuthURI = `http://www.strava.com/oauth/authorize?client_id=${this.client_id}&response_type=code&redirect_uri=http://localhost/exchange_token&approval_prompt=force&scope=activity:read_all`;
 
@@ -15,11 +16,12 @@ export class StravaAPIService {
 
   OAuth : any;
 
-  complete : boolean;
+  loading : boolean;
 
-  constructor() { 
+  constructor(private http : HttpClient) { 
     this.OAuth = null;
-    this.complete = false;
+    this.userToken = null;
+    this.loading = false;
   }
 
   OAuthPage() {
@@ -29,15 +31,76 @@ export class StravaAPIService {
   setOAuthResponse(params : any) {
     this.OAuth = params;
     if (!this.isOAuthValid()) {
-      console.log('not valid OAuth');
       return;
     }
     //OAuth is valid
-    console.log('valid OAuth');
+    this.loading = true;
+    this.getUserToken();
   }
 
   getUserToken() {
 
+    this.loading = true;
+
+    const params = new HttpParams()
+    .set('client_id', this.client_id)
+    .set('client_secret', this.client_secret)
+    .set('code', this.OAuth.code)
+    .set('grant_type', this.grant_type)
+
+    this.http.post<any>('https://www.strava.com/oauth/token', params).subscribe(
+      {
+        next: data => {
+          this.userToken = data;
+          console.log(data);
+
+          ////////////
+          //API call to add users refresh token to the user goes here:
+          console.log('To store: ' + this.userToken.refresh_token);
+          //
+          ///////////
+
+          this.getActivities();
+
+        },
+        error: err => {
+          this.userToken = null;
+        }
+      }
+    )
+
+  }
+
+  getActivities() {
+
+    //NOTE the per_page is used to regulate the size of pages returned in an array of arrays of 100 objects:
+    //i.e. 200 activities = [ [ 0 - 99 ] , [ 100 - 199 ] ]
+
+    //Api calls can be made for specific acitivity types in the future if required
+    //to get after a date add ?after=x as a parameter where x=a epoch timestamp to return all activities after the date x.
+
+    this.http.get('https://www.strava.com/api/v3/athlete/activities?per_page=200&access_token=' + this.userToken.access_token).subscribe(
+      {
+        next: data => {
+
+          //data to be sent to api:
+          console.log(data);
+
+          ////////////
+          //
+          //  API call to add users latest activities to the database go here
+          //
+          //
+          //  After: Redirection to the dashboard will occour here
+          //
+          ///////////
+
+        },
+        error: err => {
+          console.log(err);
+        }
+      }
+    )
   }
 
   isOAuthValid() : boolean | null {
@@ -50,12 +113,22 @@ export class StravaAPIService {
     return true;
   }
 
-  logOAuth() {
+  isTokenValid() : boolean {
+    if (this.userToken == null) 
+      return false;
+    return true;
+  }
+
+  logOAuth() : any {
     console.log(this.OAuth);
   }
 
-  isComplete() : boolean {
-    return false;
+  logUserToken() : any {
+    console.log(this.userToken);
+  }
+
+  isLoading() : boolean {
+    return this.loading;
   }
 
 }
--- libs/client/strava/feature/src/lib/stravalink/stravalink.component.ts
+++ libs/client/strava/feature/src/lib/stravalink/stravalink.component.ts
@@ -12,23 +12,19 @@ export class StravalinkComponent implements OnInit {
   img : string
 
   OAuthError : boolean;
+  loading : boolean;
 
   constructor(private router : Router, private strava : StravaAPIService) {
     this.img = 'https://images.unsplash.com/photo-1530143311094-34d807799e8f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2669&q=80';
     this.OAuthError = false;
     if (this.strava.isOAuthValid() == false) {
       this.OAuthError = true;
-    } else {
-      if (this.strava.isComplete()) {
-        //navigate to dashboard
-        this.router.navigate(['dashboard']);
-      } else {
-        //do nothing
-      }
     }
+    this.loading = this.strava.isLoading();
   }
 
   ngOnInit(): void {
+    console.log();
   }
 
   LinkToStrava() {
