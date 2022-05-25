d55603fe6ee5f14e35fb4b7bbd811dd85aa2af52
MatthewGotte <u20734621@tuks.co.za>
2022-05-25 11:51:29 +0200

    adding scheduleworkout lib and components

---
+++ libs/client/scheduleworkout/feature/src/lib/scheduleworkout/scheduleworkout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'training-buddy-schedule-workout',
+  templateUrl: './scheduleworkout.component.html',
+  styleUrls: ['./scheduleworkout.component.scss']
+})
+export class ScheduleworkoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
