83e9ade8de4228cc576ed2fae5632a6218432cbe
MatthewGotte <u20734621@tuks.co.za>
2022-05-29 23:33:25 +0200

    Fixing Routing

--- libs/client/strava/feature/src/lib/services/strava-api.service.spec.ts
+++ libs/client/strava/feature/src/lib/services/strava-api.service.spec.ts
@@ -1,12 +1,21 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { StravaAPIService } from './strava-api.service';
 
 describe('StravaAPIService', () => {
+  
   let service: StravaAPIService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule
+      ],
+      providers: [
+        HttpClient
+      ]
+    });
     service = TestBed.inject(StravaAPIService);
   });
 
