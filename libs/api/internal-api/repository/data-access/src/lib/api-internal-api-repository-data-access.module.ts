955c2aa7e6d337128b1e6a0f37a08c9288f804b2
Muziwandile <84350184+muzicoding@users.noreply.github.com>
2022-06-01 16:27:04 +0200

    🚧 (API/REPO) Getting firebase configs

--- libs/api/internal-api/repository/data-access/src/lib/api-internal-api-repository-data-access.module.ts
+++ libs/api/internal-api/repository/data-access/src/lib/api-internal-api-repository-data-access.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ApiInternalApiRepositoryDataAccessService } from './api-internal-api-repository-data-access.service';
-import { PrismaService } from '@training-buddy/api/shared/services/prisma//data-access';
 import * as admin from 'firebase-admin';
 
 @Module({
   controllers: [],
-  providers: [ApiInternalApiRepositoryDataAccessService, PrismaService],
+  providers: [ApiInternalApiRepositoryDataAccessService],
   exports: [ApiInternalApiRepositoryDataAccessService],
 })
 export class ApiInternalApiRepositoryDataAccessModule {}
--- libs/api/internal-api/service/training-buddy-service/src/lib/training-buddy-service.service.spec.ts
+++ libs/api/internal-api/service/training-buddy-service/src/lib/training-buddy-service.service.spec.ts
@@ -2,14 +2,13 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { TrainingBuddyServiceService } from './training-buddy-service.service';
 import { ApiInternalApiRepositoryDataAccessService } from '@training-buddy/api/internal-api/repository/data-access';
 import { JwtModule } from '@nestjs/jwt';
-import { PrismaService } from '@training-buddy/api/shared/services/prisma//data-access' ;
 import {UserEntity} from '@training-buddy/api/internal-api/api/shared/interfaces/data-access';
 describe('TrainingBuddyServiceService', () => {
   let service: TrainingBuddyServiceService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TrainingBuddyServiceService,ApiInternalApiRepositoryDataAccessService,PrismaService,UserEntity],
+      providers: [TrainingBuddyServiceService,ApiInternalApiRepositoryDataAccessService,UserEntity],
       imports: [JwtModule.register({
         signOptions: { expiresIn: '600s'},
         secret:"hide"//TODO hide this 
