490776257436f79d500741815d3931d91d35d47a
MatthewGotte <u20734621@tuks.co.za>
2022-06-02 13:06:15 +0200

    Adding schedule workout pages and updating schedule workout

--- libs/api/internal-api/api/shared/interfaces/data-access/src/lib/activity-schedule.ts
+++ libs/api/internal-api/api/shared/interfaces/data-access/src/lib/activity-schedule.ts
@@ -2,16 +2,29 @@ import { Field , Int, InputType} from '@nestjs/graphql'
 
 @InputType()
 export class ActivitySchedule {
+
+    @Field()
+    title:string
+
+    @Field()
+    description:string
+
     @Field()
     email: string
+
     @Field()
     time:string
+
     @Field()
     activity:string
+
     @Field()
     location:string
+
     @Field()
-    distance:string
+    distance:number
+
     @Field()
-    duration:number
+    speed:number
+    
 }
