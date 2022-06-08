23b4a2c7922cab8d88d153953463d5fa003fef4e
MatthewGotte <u20734621@tuks.co.za>
2022-06-09 01:18:33 +0200

    Creating Lib and Component
    Libs for profile image upload and component for the lib

---
+++ libs/client/profileimageupload/src/lib/profileupload/profileupload.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'training-buddy-profile-upload',
+  templateUrl: './profileupload.component.html',
+  styleUrls: ['./profileupload.component.scss']
+})
+export class ProfileuploadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
