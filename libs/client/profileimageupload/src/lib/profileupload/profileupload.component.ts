1d371e302c4a603ea8bdf6701b2aabb61915a764
takumuguti <90533439+takumuguti@users.noreply.github.com>
2022-06-09 03:39:31 +0200

    ✅temporary fix

--- libs/client/profileimageupload/src/lib/profileupload/profileupload.component.ts
+++ libs/client/profileimageupload/src/lib/profileupload/profileupload.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+// import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'training-buddy-profile-upload',
-  templateUrl: './profileupload.component.html',
-  styleUrls: ['./profileupload.component.scss']
-})
-export class ProfileuploadComponent implements OnInit {
+// @Component({
+//   selector: 'training-buddy-profile-upload',
+//   templateUrl: './profileupload.component.html',
+//   styleUrls: ['./profileupload.component.scss']
+// })
+// export class ProfileuploadComponent implements OnInit {
 
-  constructor() { }
+//   constructor() { }
 
-  ngOnInit(): void {
-  }
+//   ngOnInit(): void {
+//   }
 
-}
+// }
