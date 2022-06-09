97c16ad85e813b53a63a09a06fee46620a11c39b
MatthewGotte <u20734621@tuks.co.za>
2022-06-09 22:44:59 +0200

    needs get logs query

--- libs/client/viewprofile/feature/src/lib/viewprofilepage/viewprofilepage.component.ts
+++ libs/client/viewprofile/feature/src/lib/viewprofilepage/viewprofilepage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
 
 @Component({
   selector: 'training-buddy-profile-page',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewprofilepageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private apollo : Apollo) { }
 
   ngOnInit(): void {
+    this.getActivityLogs().then(res => {
+      console.log(res);
+    });
+  }
+
+  getActivityLogs() {
+    return new Promise((resolve, _) => {
+      if (!(this.apollo.client === undefined))
+      this.apollo
+        .mutate ({
+          mutation: gql`
+            
+          `,
+        })
+        .subscribe ((result) => {
+         const res: any  = result
+          resolve(res.data.signup.message);
+        });
+    });
   }
 
 }
