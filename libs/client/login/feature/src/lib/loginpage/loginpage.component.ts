a4549e5b0bab80529d36466de66010d248d33c99
takumuguti <90533439+takumuguti@users.noreply.github.com>
2022-06-09 03:12:06 +0200

    ✨Set browser cookies

--- libs/client/login/feature/src/lib/loginpage/loginpage.component.ts
+++ libs/client/login/feature/src/lib/loginpage/loginpage.component.ts
@@ -2,8 +2,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import {Apollo, gql} from 'apollo-angular';
-
-
+import {CookieService} from 'ngx-cookie-service';
 @Component({
   selector: 'training-buddy-loginpage',
   templateUrl: './loginpage.component.html',
@@ -18,7 +17,7 @@ export class LoginpageComponent implements OnInit {
   loginFrm! : FormGroup;
   frmBuilder! : FormBuilder;
 
-  constructor(private frm : FormBuilder, private apollo : Apollo, @Inject(Router) private router : Router) {
+  constructor(private frm : FormBuilder, private apollo : Apollo, @Inject(Router) private router : Router, private cookieService:CookieService) {
     this.img = 'https://images.unsplash.com/photo-1530143311094-34d807799e8f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2669&q=80';
   
     //injections
@@ -100,14 +99,20 @@ export class LoginpageComponent implements OnInit {
         
       }else{
         //store cookies
-
+        this.setCookie();
         //route.
         this.router.navigate(['/dashboard']);
       }
     })
   }
   ///////////////////////
-
+  setCookie(){
+    this.cookieService.set('email',this.userEmail);
+  }
+   
+  deleteCookie(){
+    this.cookieService.delete('email');
+  }
 }
 export interface userData{
   login:{
