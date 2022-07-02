4a832d034a85029474077fd11bf38b16f806c0ec
shannonlnoel <matthewgotte@gmail.com>
2022-07-03 00:33:19 +0200

    update const

--- libs/client/uploadprofileimage/feature/src/lib/uploadprofileimage/uploadprofileimage.component.ts
+++ libs/client/uploadprofileimage/feature/src/lib/uploadprofileimage/uploadprofileimage.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadprofileimageComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    console.log();
+
+  }
 
   ngOnInit(): void {
+    console.log();
   }
 
 }
