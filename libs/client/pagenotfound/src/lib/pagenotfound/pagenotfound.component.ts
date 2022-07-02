59a3711df1e2b9974eba4c8923a09c9fedc1e699
shannonlnoel <matthewgotte@gmail.com>
2022-07-03 00:33:19 +0200

    update const

--- libs/client/pagenotfound/src/lib/pagenotfound/pagenotfound.component.ts
+++ libs/client/pagenotfound/src/lib/pagenotfound/pagenotfound.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagenotfoundComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    console.log();
+   }
 
   ngOnInit(): void {
+    console.log();
   }
 
 }
