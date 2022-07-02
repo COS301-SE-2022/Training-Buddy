fa5038e5aa4801c656a596bed7787502969d0861
shannonlnoel <matthewgotte@gmail.com>
2022-07-03 00:00:28 +0200

    Adding Service lib
    - AuthGaurd Service
    - Repo Service for API calls

---
+++ libs/client/services/feature/README.md
@@ -0,0 +1,7 @@
+# client-services-feature
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test client-services-feature` to execute the unit tests.
---
+++ libs/client/services/feature/src/lib/repository/repo.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RepoService {
+
+  //inject the service into a constructor:
+    //private repo: RepoService
+    //disable the lint for the error on the import
+
+  constructor() { 
+    //
+  }
+
+  //repo API calls to me moved to this service file
+
+}
