18899350a09ac75dfbb59ab08a7c6b37eac1d774
shannonlnoel <matthewgotte@gmail.com>
2022-07-02 13:18:58 +0200

    adding page not found

---
+++ libs/client/uploadprofileimage/feature/src/lib/uploadprofileimage/uploadprofileimage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'training-buddy-upload-profile-image',
+  templateUrl: './uploadprofileimage.component.html',
+  styleUrls: ['./uploadprofileimage.component.scss']
+})
+export class UploadprofileimageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
