2adba64f1cd872d697465e50186786e94c60d4bc
MatthewGotte <matthewgotte@gmail.com>
2022-07-06 12:49:56 +0200

    fixing imports

--- libs/client/uploadprofileimage/feature/src/lib/uploadprofileimage/uploadprofileimage.component.spec.ts
+++ libs/client/uploadprofileimage/feature/src/lib/uploadprofileimage/uploadprofileimage.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { UiModule } from '@training-buddy/client/shared/components/navbar/ui';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
 import { UploadprofileimageComponent } from './uploadprofileimage.component';
+import { Apollo } from 'apollo-angular';
+import { CookieService } from 'ngx-cookie-service';
 
 describe('UploadprofileimageComponent', () => {
   let component: UploadprofileimageComponent;
@@ -8,7 +13,17 @@ describe('UploadprofileimageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UploadprofileimageComponent ]
+      declarations: [ UploadprofileimageComponent ],
+      imports: [
+        ReactiveFormsModule,
+        UiModule,
+        BrowserAnimationsModule,
+        RouterTestingModule
+      ],
+      providers: [
+        Apollo,
+        CookieService
+      ]
     })
     .compileComponents();
   });
