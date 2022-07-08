7362fc9c4f2abf4a1fd65dcddcb2dbbaa1774979
MatthewGotte <matthewgotte@gmail.com>
2022-07-08 22:42:42 +0200

    Adding changepassword component

--- libs/client/changepassword/feature/src/lib/client-changepassword-feature.module.ts
+++ libs/client/changepassword/feature/src/lib/client-changepassword-feature.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ChangepasswordComponent } from './changepassword/changepassword.component';
 
 @NgModule({
   imports: [
@@ -9,6 +10,9 @@ import { RouterModule } from '@angular/router';
     RouterModule.forChild([
       /* {path: '', pathMatch: 'full', component: InsertYourComponentHere} */
     ]),
+  ],
+  declarations: [
+    ChangepasswordComponent
   ],
 })
 export class ClientChangepasswordFeatureModule {}
