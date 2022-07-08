64c76afa5da41117a8da4d3ff84fc5577c4520f7
MatthewGotte <matthewgotte@gmail.com>
2022-07-08 22:42:42 +0200

    Adding changepassword component

---
+++ libs/client/changepassword/feature/src/lib/changepassword/changepassword.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'training-buddy-change-password',
+  templateUrl: './changepassword.component.html',
+  styleUrls: ['./changepassword.component.scss']
+})
+export class ChangepasswordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
