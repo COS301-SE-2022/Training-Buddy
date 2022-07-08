63ad6f1b3c09cab89a1d3330c9daf4c157d45682
MatthewGotte <matthewgotte@gmail.com>
2022-07-09 00:07:33 +0200

    change passwords ready for API

--- libs/client/changepassword/feature/src/lib/changepassword/changepassword.component.spec.ts
+++ libs/client/changepassword/feature/src/lib/changepassword/changepassword.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { UiModule } from '@training-buddy/client/shared/components/navbar/ui';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
 import { ChangepasswordComponent } from './changepassword.component';
 
 describe('ChangepasswordComponent', () => {
@@ -8,7 +11,13 @@ describe('ChangepasswordComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ChangepasswordComponent ]
+      declarations: [ ChangepasswordComponent ],
+      imports:[
+        ReactiveFormsModule,
+        UiModule,
+        BrowserAnimationsModule,
+        RouterTestingModule
+      ]
     })
     .compileComponents();
   });
--- libs/client/changepassword/feature/src/lib/client-changepassword-feature.module.ts
+++ libs/client/changepassword/feature/src/lib/client-changepassword-feature.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChangepasswordComponent } from './changepassword/changepassword.component';
 import { ChangePasswordRoutingModule } from './changepassword-routing.module';
+import { UiModule } from '@training-buddy/client/shared/components/navbar/ui';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    ChangePasswordRoutingModule
+    ChangePasswordRoutingModule,
+    UiModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   declarations: [
     ChangepasswordComponent
