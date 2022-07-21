f8fa5bba03e43f0846028ef58d7ea3b0d2328456
takumuguti <90533439+takumuguti@users.noreply.github.com>
2022-07-21 14:47:43 +0200

    💅🏾 fix css

--- libs/client/schedule/feature/src/lib/viewschedule/viewschedule.component.ts
+++ libs/client/schedule/feature/src/lib/viewschedule/viewschedule.component.ts
@@ -119,7 +119,6 @@ export class ViewscheduleComponent implements OnInit {
 
   startDateTime(data: string): any{
     //write a function that returns the date and time 
-    // e.g 13:00
     const date = new Date(Number(data) * 1000);
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
    
