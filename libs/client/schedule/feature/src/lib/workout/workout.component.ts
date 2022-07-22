f0f0e0da3feccaf6c53fd4be023a75602d494f78
takumuguti <90533439+takumuguti@users.noreply.github.com>
2022-07-22 13:53:08 +0200

    create workout component

---
+++ libs/client/schedule/feature/src/lib/workout/workout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'training-buddy-workout',
+  templateUrl: './workout.component.html',
+  styleUrls: ['./workout.component.scss']
+})
+export class WorkoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
