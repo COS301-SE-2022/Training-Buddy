ee5fa8e54b6daaf17fde809ed23573733072a471
takumuguti <90533439+takumuguti@users.noreply.github.com>
2022-07-24 19:58:17 +0200

    workout component

--- libs/client/schedule/feature/src/lib/workout/workout.component.ts
+++ libs/client/schedule/feature/src/lib/workout/workout.component.ts
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkoutComponent implements OnInit {
 
-  constructor() { }
+  // constructor() { }
 
-  ngOnInit(): void {
-  }
+  // ngOnInit(): void {
+  // }
 
 }
