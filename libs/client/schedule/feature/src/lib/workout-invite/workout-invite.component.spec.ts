7ba929e68763bc0878b89d8661dba1e8e95f5694
takumuguti <90533439+takumuguti@users.noreply.github.com>
2022-07-28 19:40:26 +0200

    ✅ update providers

--- libs/client/schedule/feature/src/lib/workout-invite/workout-invite.component.spec.ts
+++ libs/client/schedule/feature/src/lib/workout-invite/workout-invite.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { UiModule } from '@training-buddy/client/shared/components/navbar/ui';
 import { Apollo } from 'apollo-angular';
 import { CookieService } from 'ngx-cookie-service';
@@ -19,6 +19,7 @@ describe('WorkoutInviteComponent', () => {
       providers: [
         Apollo,
         CookieService,
+        MatDialogRef
       ]
     })
     .compileComponents();
