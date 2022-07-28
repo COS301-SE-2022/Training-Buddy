a0e28f80eaf5016ec97c15c9edcb2d80bfb1a008
MatthewGotte <u20734621@tuks.co.za>
2022-07-28 23:16:32 +0200

    working accept incoming

--- libs/client/dashboard/feature/src/lib/dashboard/dashboard.component.ts
+++ libs/client/dashboard/feature/src/lib/dashboard/dashboard.component.ts
@@ -1,14 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
 import { Apollo, gql } from 'apollo-angular';
 import { CookieService } from 'ngx-cookie-service';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { tap } from 'rxjs';
-import { resolve } from 'path';
+import { animate, keyframes, style, transition, trigger } from '@angular/animations';
 @Component({
   selector: 'training-buddy-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+  animations: [
+    trigger(
+      'fadeIn', [
+        transition(':enter', [
+          animate(200, keyframes([
+            style({
+              opacity: '0'
+            }),
+            style({
+              opacity: '1'
+            })
+          ]))
+        ]),
+        transition(':leave', [
+          animate(300, keyframes([
+            style({
+              opacity: '1'
+            }),
+            style({
+              opacity: '0'
+            })
+          ]))
+        ])
+      ],
+    )
+  ]
 })
 
 export class DashboardComponent implements OnInit {
@@ -46,29 +72,78 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit() {
 
+    //getting rec. buddies from engine
     this.getBuddieRecommended().subscribe({
       next: async (data : any) => {
-        const d = data/
-        // const filter = this.removeOverlapConnections(data.data.findAll);
-        // await this.fetchImages(filter);
-        // await this.fetchImages(filter).then((o) => {
-        //   this.buddies = o;
-        //   if (this.buddies.length != 0)
-        //     this.noBuddies = false;
-        //   this.doneloading = true;
-        // })
-        // this.oldBuddies = await this.fetchImages(filter);
+        const filter = this.removeOverlapConnections(data.data.findAll);
+        await this.fetchImages(filter).then((o : any[]) => {
+          this.buddies = o;
+          this.oldBuddies = o;
+          console.log('buddies', this.buddies)
+          if (this.buddies.length == 0)
+            this.noBuddies = false;
+            console.log('onoBuddies', this.noBuddies)
+          this.doneloading = true;
+        })
       }
     });
 
-    this.firestore.collection('BuddyRequests').valueChanges().subscribe(resp => {
-      this.requests = this.getUsersFromRequests(this.filterIncoming(resp));
-      this.outgoingRequests = this.filterOutgoing(resp);
-    });
+    //logic for the incoming and dynamics on sender client
+    this.firestore
+      .collection('BuddyRequests', ref => ref.where('receiver', '==', this.email))
+      .valueChanges()
+      .subscribe((inRequests : any) => {
+        this.requests = [];
+        this.pendingrequests = false;
+        //get the users the requests came from:
+        if (inRequests.length != 0) {
+          this.pendingrequests = true;
+          inRequests.forEach((req : any) => {
+            this.firestore
+            .collection('Users', ref => ref.where('email', '==', req.sender))
+            .valueChanges()
+            .subscribe((incomingUsrs : any) => {
+              this.requests = [];
+              this.removeOverlapConnections(incomingUsrs).forEach((usr : any) => {
+                this.fetchSingleImage(usr).then((imgUsr : any) => {
+                  this.requests.push(imgUsr);
+                }); 
+              });
+            })
+          })
+        }
+      })
+
+    //getting outgoing for disabling request button on current client
+    this.firestore
+      .collection('BuddyRequests', ref => ref.where('sender', '==', this.email))
+      .valueChanges()
+      .subscribe((outgoing : any) => {
+        this.buddies = this.removeOverlapConnections(this.buddies);
+        this.oldBuddies = this.removeOverlapConnections(this.oldBuddies);
+        this.outgoingRequests = outgoing;
+      });
 
   }
 
-  fetchImages(data : any) {
+  fetchSingleImage(usr : any) : Promise<any> {
+    return new Promise<any>((res, rej) => {
+      this.afStorage
+      .ref(`UserProfileImage/${usr.id}`)
+      .getDownloadURL()
+      .pipe(
+        tap((url : any) => {
+          const image = {image : url};
+          res({
+            ...usr,
+            ...image
+          });
+        })
+      ).subscribe();
+    })
+  }
+
+  fetchImages(data : any[]) : Promise<any> {
    return new Promise<any>((res, rej) => {
     const o : any[] = [];
     data.forEach((usr : any) => {
@@ -82,7 +157,6 @@ export class DashboardComponent implements OnInit {
               ...usr,
               ...image
             }
-            console.log('p', p)
             o.push(p);
           })
         ).subscribe();
@@ -91,22 +165,35 @@ export class DashboardComponent implements OnInit {
    })
   }
 
+  removeRec(usr : any) {
+    this.buddies = this.buddies.filter((b : any) => {
+      return usr.email != b.email;
+    });
+    this.oldBuddies = this.oldBuddies.filter((b : any) => {
+      return usr.email != b.email;
+    });
+  }
+
+  trackById(usr : any, index : number) {
+    return usr.id;
+  }
+
   //remove paired buddies
-  // removeOverlapConnections(data : any) : any[] {
-  //   if (data == null)
-  //     return [];
-  //   const o : any[] = [];
-  //   data.map((el : any) => {
-  //     let flag = true;
-  //     el.buddies.map((cons : any) => {
-  //       if (this.email == cons)
-  //         flag = false;
-  //     });
-  //     if (flag)
-  //     o.push(el);
-  //   });
-  //   return o;
-  // }
+  removeOverlapConnections(data : any) : any[] {
+    if (data == null)
+      return data;
+    const o : any[] = [];
+    data.map((el : any) => {
+      let flag = true;
+      el.buddies.map((cons : any) => {
+        if (this.email == cons)
+          flag = false;
+      });
+      if (flag)
+        o.push(el);
+    });
+    return o;
+  }
 
   //filter outgoing
   filterOutgoing(data : any) : any[] {
@@ -142,36 +229,6 @@ export class DashboardComponent implements OnInit {
     });
     return o;
   }
-
-  //old method
-  // getIncomingRequests() {
-  //   return this.apollo
-  //     .watchQuery({
-  //       query: gql`query{
-  //         getIncoming(
-  //           email: "${this.email}",
-  //       ){
-  //         userName,
-  //         userSurname,
-  //         location,
-  //         longitude,
-  //         latitude,
-  //         stravaToken,
-  //         dob,
-  //         gender,
-  //         email,
-  //         cellNumber,
-  //         bio,
-  //         metrics{lift , ride , run , swim},
-  //         buddies
-  //       }
-  //       }
-  //       `,
-  //       // pollInterval: 1000,
-  //       // fetchPolicy: 'network-only'
-  //     },
-  //     ).valueChanges;
-  // }
 
   getOutgoing() {
     return this.apollo
@@ -226,9 +283,11 @@ export class DashboardComponent implements OnInit {
       });
   }
 
-  checkIfInOutgoing(email : string) : boolean {
+  inOutgoing(email : string) : boolean {
+
     if (this.outgoingRequests == null)
-    return true;
+      return false;
+
     let flag = false;
     this.outgoingRequests.map((el : any) => {
       if (el.receiver == email) {
@@ -255,6 +314,11 @@ export class DashboardComponent implements OnInit {
   }
 
   accept(email : string){
+    this.requests.map((el : any, i : number) => {
+      if (el.email == email) {
+        this.requests.splice(i, 1);
+      }
+    });
     this.apollo
     .mutate({
       mutation: gql`mutation{
@@ -266,19 +330,18 @@ export class DashboardComponent implements OnInit {
       }
       }
       `,
-    }).subscribe({
-      next: (data : any) => {
-        this.requests.map((el : any, i : number) => {
-          if (el.email == email) {
-            this.requests.splice(i, 1);
-          }
-        });
-      }
-    });
+    }).subscribe();
   }
 
   reject(data : string){
-
+    this.requests.forEach((el : any, i : number) => {
+        if (el.email == data) {
+          this.requests.splice(i, 1);
+          if (this.requests.length == 0) {
+            this.pendingrequests = false;
+          }
+        }
+      })
     this.apollo
     .mutate({
       mutation: gql`mutation{
@@ -290,17 +353,7 @@ export class DashboardComponent implements OnInit {
       }
       }
       `,
-    }).subscribe(() => {
-      this.requests.forEach((el : any, i : number) => {
-        if (el.email == data) {
-          this.requests.splice(i, 1);
-          if (this.requests.length == 0) {
-            this.pendingrequests = false;
-          }
-        }
-      })
-    });
-
+    }).subscribe();
   }
   
   getFriends(){
@@ -365,19 +418,6 @@ export class DashboardComponent implements OnInit {
     this.buddies = this.buddies.filter((el : any) => {
       return el.metrics[attr] == 1;
     });
-  }
-
-  async getImage(user : any ){
-    // this.ref = this.afStorage.ref("UserProfileImage/"+user.id);
-    // this.ref.getDownloadURL()
-    // .pipe(
-    //   tap((el : any) => {
-    //     console.log(el);
-    //   })
-    // ).subscribe().unsubscribe();
-   
-    // console.log(await this.afStorage.ref("UserProfileImage/"+user.id).getDownloadURL().);
-
   }
 
 }
