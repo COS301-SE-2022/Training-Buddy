19411a592797acdc32830a49a7287a4619cf2b49
Lehlohonolo-Sehako <u20557622@tuks.co.za>
2022-07-28 21:29:04 +0200

    🚧 ✅ (Integration): tested update functionality

--- cypress/integration/7.activitysettings.spec.ts
+++ cypress/integration/7.activitysettings.spec.ts
@@ -10,7 +10,24 @@ describe.only('training-buddy-client-athlete-profile-feature', () => {
         
     });
 
-    it.only('should add all activites', () => {
+    it('should add one activity only', () => {
+        
+        cy.get('#mat-checkbox-3 > .mat-checkbox-layout > .mat-checkbox-inner-container').click()
+
+        cy.get('#signup').click()
+
+    });
+
+    it('should add user bio only', () => {
+
+        cy.get('#mat-input-0').type('I love running and riding, but would also like to get into swimming and weight lifting I am also a student @ Tuks')
+
+        cy.get('#signup').click()
+        
+    });
+
+
+    it('should add all activites', () => {
         
         cy.get('#mat-checkbox-1 > .mat-checkbox-layout > .mat-checkbox-inner-container').click()
 
