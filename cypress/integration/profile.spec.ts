6758e77fd31a8a95f735821d4d689ad9f14c9049
Lehlohonolo-Sehako <u20557622@tuks.co.za>
2022-07-28 16:15:39 +0200

    🔒️ ✅ (Integration): Tested athlete profile

---
+++ cypress/integration/profile.spec.ts
@@ -0,0 +1,24 @@
+
+describe.only('training-buddy-client-athlete-profile-feature', () => {
+    
+    before(() => {
+        cy.visit('/profile')  
+    })
+
+    it('should allow user to navigate to and from profile', () => {
+
+        cy.get(':nth-child(2) > .link').click()
+
+        cy.get(':nth-child(1) > .link').click()
+
+        cy.get('#configurelink > .ng-tns-c163-0').click()
+
+        cy.get('[ng-reflect-router-link="/profile"]').click()
+
+        cy.get('.rightcontent > .mat-icon').click()
+
+        cy.get('.mat-toolbar-row > .mat-icon').click()
+        
+    });
+
+})
