f96e8b37bb0c5864bca17ddee1554489b1b36b9e
Lehlohonolo-Sehako <u20557622@tuks.co.za>
2022-07-28 21:22:41 +0200

    🚧 ✅ (Integration): Tested activity settings

--- cypress/integration/7.activitysettings.spec.ts
+++ cypress/integration/7.activitysettings.spec.ts
@@ -1,8 +1,34 @@
-describe('training-buddy-client-athlete-profile-feature', () => {
+describe.only('training-buddy-client-athlete-profile-feature', () => {
 
     before(() => {
         cy.visit('/configureprofile/update') 
     })
+
+    it('should try to save without updates', () => {
+
+        cy.get('#signup').click()
+        
+    });
+
+    it.only('should add all activites', () => {
+        
+        cy.get('#mat-checkbox-1 > .mat-checkbox-layout > .mat-checkbox-inner-container').click()
+
+        cy.get('#mat-checkbox-2 > .mat-checkbox-layout > .mat-checkbox-inner-container').click()
+
+        cy.get('#mat-checkbox-3 > .mat-checkbox-layout > .mat-checkbox-inner-container').click()
+
+        cy.get('#mat-checkbox-4 > .mat-checkbox-layout > .mat-checkbox-inner-container').click()
+
+
+        cy.get('.mat-slider-thumb')
+        .type("{rightarrow}{rightarrow}{rightarrow}{rightarrow}{rightarrow}{rightarrow}{rightarrow}");
+
+        cy.get('#mat-input-0').type('I love running and riding, but would also like to get into swimming and weight lifting I am also a student @ Tuks')
+
+        cy.get('#signup').click()
+
+    });
     
 
 });
