e7c23939825d88a5ccc52b2a890d2b47325a4a5e
takumuguti <90533439+takumuguti@users.noreply.github.com>
2022-07-28 19:54:51 +0200

    ✅ update providers

--- libs/client/schedule/feature/src/lib/workout/workout.component.spec.ts
+++ libs/client/schedule/feature/src/lib/workout/workout.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FIREBASE_OPTIONS } from '@angular/fire/compat';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatDialog, MatDialogModule, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { RouterTestingModule } from '@angular/router/testing';
 import { UiModule } from '@training-buddy/client/shared/components/navbar/ui';
 import { Apollo } from 'apollo-angular';
@@ -24,6 +24,7 @@ describe('WorkoutComponent', () => {
       providers: [
         Apollo,
         MatDialog,
+        MAT_DIALOG_DATA,
         CookieService,
         { provide: FIREBASE_OPTIONS, useValue:firebase }
       ],
