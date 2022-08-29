e4071e2d34fec88c8e49a1515bca83574160e8b2
Takudzwanashe Dylan Muguti <90533439+takumuguti@users.noreply.github.com>
2022-08-29 11:21:34 +0200

    ✅update testing for the rating system

--- libs/client/schedule/feature/src/lib/rating/rating.component.spec.ts
+++ libs/client/schedule/feature/src/lib/rating/rating.component.spec.ts
@@ -1,4 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { UiModule } from '@training-buddy/client/shared/components/navbar/ui';
+import { Apollo } from 'apollo-angular';
 
 import { RatingComponent } from './rating.component';
 
@@ -8,7 +13,24 @@ describe('RatingComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RatingComponent ]
+      declarations: [ RatingComponent ],
+      imports: [
+        UiModule,
+        MatDialogModule,
+        BrowserAnimationsModule,
+        RouterTestingModule
+      ],
+      providers: [
+        Apollo,
+        {
+          provide: MatDialogRef,
+          useValue: []
+        },
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: [],
+        }
+      ]
     })
     .compileComponents();
   });
