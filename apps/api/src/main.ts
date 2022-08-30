8dd8ccb0b60d5870d4e727db2dd58dae3841ad3d
sarah-a-king <94594759+sarah-a-king@users.noreply.github.com>
2022-08-30 21:16:42 +0200

    webhooks working! (basically)

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -41,6 +41,39 @@ async function bootstrap() {
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
+
+  const webhookServer = express() ;
+
+  // Sets server port and logs message on success
+  webhookServer.listen(process.env.PORT || 3000, () => console.log('webhook is listening'));
+
+  // Creates the endpoint for our webhook
+  webhookServer.post('/webhook', (req, res) => {
+    console.log("webhook event received!", req.query, req.body);
+    res.status(200).send('EVENT_RECEIVED');
+  });
+
+  // Adds support for GET requests to our webhook
+  webhookServer.get('/webhook', (req, res) => {
+    // Your verify token. Should be a random string.
+    const VERIFY_TOKEN = "STRAVA";
+    // Parses the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+      // Verifies that the mode and token sent are valid
+      if (mode === 'subscribe' && token === VERIFY_TOKEN) {     
+        // Responds with the challenge token from the request
+        console.log('WEBHOOK_VERIFIED');
+        res.json({"hub.challenge":challenge});  
+      } else {
+        // Responds with '403 Forbidden' if verify tokens do not match
+        res.sendStatus(403);      
+      }
+    }
+  });
 }
 
 bootstrap();
