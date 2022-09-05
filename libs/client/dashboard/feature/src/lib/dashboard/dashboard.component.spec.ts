5706472b3b38db301d60d8753c37675cb2c246f9
Takudzwanashe Dylan Muguti <90533439+takumuguti@users.noreply.github.com>
2022-09-05 10:21:13 +0200

    ✅update dashboard test

--- libs/client/dashboard/feature/src/lib/dashboard/dashboard.component.spec.ts
+++ libs/client/dashboard/feature/src/lib/dashboard/dashboard.component.spec.ts
@@ -6,9 +6,18 @@ import { Apollo } from 'apollo-angular';
 import { CookieService } from 'ngx-cookie-service';
 import { ReactiveFormsModule } from '@angular/forms';
 import { UiModule } from '@training-buddy/client/shared/components/navbar/ui';
-import { FIREBASE_OPTIONS } from '@angular/fire/compat';
+import { AngularFireModule, FIREBASE_OPTIONS } from '@angular/fire/compat';
 import firebase = require('firebase/compat');
-
+const firebaseConfig = {
+  apiKey: 'AIzaSyD_61N0OLPsfAKHoawzDtIExK_BU3GR6hM',
+  authDomain: 'training-buddy-2022.firebaseapp.com',
+  databaseURL: 'https://training-buddy-2022-default-rtdb.firebaseio.com',
+  projectId: 'training-buddy-2022',
+  storageBucket: 'training-buddy-2022.appspot.com',
+  messagingSenderId: '<your-messaging-sender-id>',
+  appId: '445917436',
+  measurementId: 'G-K7WPZTL3FJ'
+}
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
@@ -20,7 +29,8 @@ describe('DashboardComponent', () => {
         ReactiveFormsModule,
         UiModule,
         BrowserAnimationsModule,
-        RouterTestingModule
+        RouterTestingModule,
+        AngularFireModule.initializeApp(firebaseConfig)
       ],
       providers: [
         Apollo,
