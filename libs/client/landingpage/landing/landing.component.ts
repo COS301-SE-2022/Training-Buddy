ae0ace9ddccb305c6810d1858bee1ae84aa9fef0
MatthewGotte <u20734621@tuks.co.za>
2022-09-13 12:35:16 +0200

    landing page component

---
+++ libs/client/landingpage/landing/landing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'training-buddy-landing',
+  templateUrl: './landing.component.html',
+  styleUrls: ['./landing.component.scss']
+})
+export class LandingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
