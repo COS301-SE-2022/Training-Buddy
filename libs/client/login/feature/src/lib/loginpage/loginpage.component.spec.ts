f1438d1e4773d81cd54c3a18705d164638232e1d
Lehlohonolo-Sehako <u20557622@tuks.co.za>
2022-09-16 11:12:48 +0200

    ✅ (Coverage)

--- libs/client/login/feature/src/lib/loginpage/loginpage.component.spec.ts
+++ libs/client/login/feature/src/lib/loginpage/loginpage.component.spec.ts
@@ -1,15 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, Validators } from '@angular/forms';
 import { UiModule } from '@training-buddy/client/shared/components/navbar/ui';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { LoginpageComponent } from './loginpage.component';
-import { Apollo } from 'apollo-angular';
+import { Apollo, gql } from 'apollo-angular';
 import {CookieService} from 'ngx-cookie-service';
+
 
 describe('LoginpageComponent', () => {
   let component: LoginpageComponent;
   let fixture: ComponentFixture<LoginpageComponent>;
+
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -18,7 +20,8 @@ describe('LoginpageComponent', () => {
         ReactiveFormsModule,
         UiModule,
         BrowserAnimationsModule,
-        RouterTestingModule
+        RouterTestingModule,
+
       ],
       providers: [
         Apollo,
@@ -26,6 +29,7 @@ describe('LoginpageComponent', () => {
       ]
     })
     .compileComponents();
+
   });
 
   beforeEach(() => {
@@ -34,7 +38,81 @@ describe('LoginpageComponent', () => {
     fixture.detectChanges();
   });
 
+
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  /**
+   * Test ngOnInit Function
+   */
+  describe('ngOnInit', () => {
+    it('should create form', () => {
+      component.ngOnInit();
+      
+      component.loginFrm = component.frmBuilder.group({
+        userEmail : ['', Validators.required],
+        userPassword : ['', Validators.required]
+      });
+    
+      component.loginFrm.controls['userEmail'].setValue('tester@gmail.com');
+      component.loginFrm.controls['userPassword'].setValue('password');
+
+      expect(component.loginFrm.valid).toBeTruthy();
+
+    });
+  });
+
+  /**
+   * Test login Function
+   */
+  describe('login', () => {
+    it('should allow user to login', () => {
+
+      jest.spyOn(component, 'login').mockImplementation(() => {
+        component.loginFrm.controls['userEmail'].setValue('tester@gmail.com');
+        component.loginFrm.controls['userPassword'].setValue('password');
+        component.login();
+      })
+
+      expect(component.login).toBeTruthy();
+
+    });
+  });
+
+  /**
+   * Test queryLogin functionality
+   */
+  describe('queryLogin', () => {
+    it('should successfully return login query', async () => {
+        
+      //Apollo create
+      const apollo = TestBed.inject(Apollo);
+
+      return apollo.mutate({
+        mutation: gql`
+        mutation{
+          login(loginInput:{
+            username: "tester",
+            password: "password"
+          }){
+            user{
+              userName,
+              userSurname,
+              email,
+              id
+            }
+          }
+        }
+        `,
+      }).subscribe((result) => {
+        expect(result).toEqual({ data: { login: { username: 'tester', password: 'password' } } });
+      });
+
+    });
+
+  });
+
+
 });
