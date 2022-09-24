22643eefb4ce0362f96701d608c55411a2d1eaae
Lehlohonolo-Sehako <u20557622@tuks.co.za>
2022-09-24 21:43:45 +0200

    🐛 ✅ (Unit): test validate function

--- libs/api/internal-api/service/training-buddy-service/src/lib/local.strategy.spec.ts
+++ libs/api/internal-api/service/training-buddy-service/src/lib/local.strategy.spec.ts
@@ -22,4 +22,41 @@ describe('LocalStrategy', () => {
   it('should be defined', () => {
     expect(provider).toBeDefined();
   });
+
+  /**
+   * Test validate function
+   */
+  describe('validate', () => {
+    it('should return a user', () => {
+
+      //Mock user
+      const user: any = {
+        email: 'tester@gmail.com',
+        password: '123456',
+      };
+
+
+      jest.spyOn(provider, 'validate').mockImplementation(() => Promise.resolve(user));
+
+      expect(provider.validate(user.email, user.password)).resolves.toEqual(user);
+      
+    });
+
+    it('should return user has no account error', () => {
+
+      const user: any = {
+        email: 'tester@gmail.com',
+        password: '123456',
+      };
+
+      jest.spyOn(provider, 'validate').mockImplementation(() => Promise.resolve('User has no Account'));
+
+      expect(provider.validate(user.email, user.password)).resolves.toEqual('User has no Account');
+      
+
+    });
+
+  });
+
+
 });
