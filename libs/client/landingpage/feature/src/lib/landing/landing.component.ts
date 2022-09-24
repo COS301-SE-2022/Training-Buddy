b6dd7ce57acd62e66ed8e916b0eb4ef5087d5c40
Lehlohonolo-Sehako <u20557622@tuks.co.za>
2022-09-24 16:47:12 +0200

    💄 (landing): refactor landing

--- libs/client/landingpage/feature/src/lib/landing/landing.component.ts
+++ libs/client/landingpage/feature/src/lib/landing/landing.component.ts
@@ -7,10 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  img : string;
-
   constructor() {
-    this.img = 'https://images.unsplash.com/photo-1461897104016-0b3b00cc81ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80';
+    return;
   }
 
   ngOnInit(): void {
