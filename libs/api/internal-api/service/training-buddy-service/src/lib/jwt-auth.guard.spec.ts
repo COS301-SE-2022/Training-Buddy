6c722255ff188b573542485c6241c7a21af44e01
Lehlohonolo-Sehako <u20557622@tuks.co.za>
2022-09-24 21:47:03 +0200

    ✅ (Integration): test getRequest using authGuard

--- libs/api/internal-api/service/training-buddy-service/src/lib/jwt-auth.guard.spec.ts
+++ libs/api/internal-api/service/training-buddy-service/src/lib/jwt-auth.guard.spec.ts
@@ -4,4 +4,35 @@ describe('JwtAuthGuard', () => {
   it('should be defined', () => {
     expect(new JwtAuthGuard()).toBeDefined();
   });
+
+  /**
+   * Test getRequest function
+   */
+  describe('getRequest', () => {
+    it('should return request body', () => {
+      const context: any = {
+        getContext: () => {
+          return {
+            req: {
+              body: {
+                email: 'tester@gmail.com',
+                password: '123456',
+              },
+            },
+          };
+        },
+      };
+
+      const guard = new JwtAuthGuard();
+
+      jest.spyOn(guard, 'getRequest').mockImplementation(() => context.getContext().req);
+
+      expect(guard.getRequest(context)).toEqual({
+        body: {
+          email: 'tester@gmail.com',
+          password: '123456',
+        },
+      });
+    });
+  });
 });
