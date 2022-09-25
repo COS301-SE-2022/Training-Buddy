fba91dac4cac439622382bf05fcbfebe5568586f
Muziwandile <84350184+muzicoding@users.noreply.github.com>
2022-09-25 15:11:30 +0200

    ✨ dynamic Import

--- libs/api/internal-api/service/training-buddy-service/src/lib/training-buddy-service.service.ts
+++ libs/api/internal-api/service/training-buddy-service/src/lib/training-buddy-service.service.ts
@@ -3,22 +3,27 @@ import {UserDto , UserEntity,ActivitySchedule,  ErrorMessage, ActivityStat,Activ
 import {JwtService} from '@nestjs/jwt'
 import * as bcrypt from 'bcrypt';
 import { sha256 } from 'js-sha256';
-const  SibApiV3Sdk = require('sib-api-v3-sdk');
-const defaultClient = SibApiV3Sdk.ApiClient.instance;
+
 import { ApiInternalApiRepositoryDataAccessService } from '@training-buddy/api/internal-api/repository/data-access';
 let recommended : any [] =[]
 @Injectable()
 export class TrainingBuddyServiceService {
+   
+   
+    
+
     /**
      * 
      * @param jwtService 
      */
     constructor(private jwtService : JwtService, private repoService : ApiInternalApiRepositoryDataAccessService , private user : UserEntity){}
     async sendEmail(email : string , user : UserEntity) {
+        const  SibApiV3Sdk = await import ('sib-api-v3-sdk');
+        const defaultClient = SibApiV3Sdk.ApiClient.instance;
         const apiKey = defaultClient.authentications['api-key'];
         apiKey.apiKey =process.env.SENDINBLUE_API_KEY;
         const  apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
-        let  sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+        let  sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail(); // SendSmtpEmail | Values to send a transactional email
         const rec = await this.findOne(email);
         sendSmtpEmail = {
             to: [{
