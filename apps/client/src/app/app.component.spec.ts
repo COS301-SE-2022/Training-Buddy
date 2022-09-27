a11b278ec75ec739aaa4bf20900a28ee0f7da09c
Lehlohonolo-Sehako <u20557622@tuks.co.za>
2022-09-27 05:59:03 +0200

    ✅ (Routing): provide tests for routing files

--- apps/client/src/app/app.component.spec.ts
+++ apps/client/src/app/app.component.spec.ts
@@ -3,6 +3,8 @@ import { AppComponent } from './app.component';
 // import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { FeatureModule as ClientShellFeatureModule } from '@training-buddy/client/shell/feature';
+import { AuthGaurdService } from './auth-guard.service';
+import { Router } from '@angular/router';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -18,6 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
+
   it(`should have as title 'client'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
@@ -32,4 +35,84 @@ describe('AppComponent', () => {
   //     'Welcome client'
   //   );
   // });
+
+  /**
+   * Test constructor
+   */
+  describe('constructor', () => {
+    it('should subscribe to authStatus', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+
+      const auth = TestBed.inject(AuthGaurdService);
+
+      const router = TestBed.inject(Router);
+
+      auth.authStatus.subscribe((val) => {
+        if (!val) {
+          router.navigate(['/login']);
+
+          expect(router.navigate).toHaveBeenCalledWith(['/login']);
+        }
+      });
+
+      expect(auth.authStatus).toBeTruthy();
+    });
+  });
+
+
+
+  /**
+   * Test can activate function
+   */
+  describe('canActivate', () => {
+    it('should return router login', () => {
+
+      const auth = TestBed.inject(AuthGaurdService);
+      const router: Router = TestBed.inject(Router);
+      let result = router.navigate(['/']);
+
+      auth.state = false; 
+      
+      auth.canActivate();
+
+      expect(result).toEqual(router.navigate(['/login']));
+
+    });
+  });
+
+  /**
+   * Test logIn function
+   */
+  describe('logIn', () => {
+    it('should return true', () => {
+
+      const auth = TestBed.inject(AuthGaurdService);
+
+      auth.state = false;
+
+      auth.logIn();
+
+      expect(auth.state).toEqual(true);
+
+
+    });
+  });
+
+  /**
+   * Test logOut function
+   */
+  describe('logOut', () => {
+    it('should return false', () => {
+
+      const auth = TestBed.inject(AuthGaurdService);
+
+      auth.state = true;
+
+      auth.logOut();
+
+      expect(auth.state).toEqual(false);
+
+    });
+  });
+
 });
